dffca1b13a176ddf68d5183b4537adef1d35d24b
Filipe Souza <lipeapostolo@yahoo.com.br>
2023-08-19 23:33:11 -0300

    versão 2 main.py

--- main.py
+++ main.py
@@ -1,65 +1,48 @@
-LIMITE_VALOR_SAQUE = 500
-LIMITE_SAQUES = 3
-saldo = 0
-extrato = []
-numero_saque = 0
-
-menu = """
-****** MENU ******
-
-[d] Depositar
-[s] Saque
-[e] Extrato
-[q] Sair
-******************
-"""
-def solicita_valor():
-    valor_inputado = float(input("Digite Valor:"))
-    print("\n")
-    return valor_inputado
-
-def verifica_valor(valor):
-    nao_valido = valor <= 0
-    
-    if nao_valido:
-        print("Digite um valor válido.")
-    
-    return nao_valido
-
-def deposita():
-    global saldo
+#Passgem parametro por posição
+def deposita(saldo, extrato, /):
     valor = solicita_valor()
-
     if verifica_valor(valor):
-       return
-    
+        print("\n### Operação falhou! O valor informado é inválido. ###")
+        return
+
     saldo += valor
-    resgistra_operacao(valor, True)
+    extrato.append(f"Depósito: R$ {valor:.2f}")
+    print("\n*** Depósito realizado com sucesso! ***") 
     
-def saca():
-    global saldo
+    return saldo, extrato
+
+#Passagem por nome
+def saca(*, saldo, extrato, limite, numero_saques, limite_saques):
     valor = solicita_valor()
-
     if verifica_valor(valor):
-        return
+        return saldo, extrato, numero_saques
     
-    if numero_saque >= LIMITE_SAQUES:
-        print("Limite saque atingido.")
-        return
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("#### Você não tem saldo suficiente. ####")
+        return saldo, extrato, numero_saques
+
+    elif excedeu_limite:
+        print("#### O valor do saque excede o limite. ####")
+        return saldo, extrato, numero_saques
+
+    elif excedeu_saques:
+        print("#### Número máximo de saques excedido. ####")
+        return saldo, extrato, numero_saques
     
-    if valor > saldo:
-        print("Não possui saldo suficiente.")
-        return
-
-    if valor > LIMITE_VALOR_SAQUE:
-        print("O valor ultrapassou o limete do saque")
-        return
-
     saldo -= valor
-    resgistra_operacao(valor, False)
+    numero_saques += 1
+    print("### Saque realizado com sucesso! ###")
+    extrato.append(f"R$ -{valor:,.2f}")
 
-def imprime_extrato():
+    return saldo, extrato, numero_saques
 
+#Passagem do parametro posição e por nome
+def imprime_extrato(saldo, /, *, extrato):
+    print("*********** EXTRATO ***********")
     if len(extrato) == 0:
         print("Não foram realizadas movimentações.")
 
@@ -67,32 +50,98 @@ def imprime_extrato():
         print(operacao)
 
     print(f"Saldo atual R$ {saldo:,.2f}")
+    print("*******************************")
 
-def resgistra_operacao(valor, deposito):
-    global numero_saque
+def solicita_valor() -> float:
+    valor_inputado = float(input("Digite Valor:"))
+    print("\n")
+    return valor_inputado
 
-    if deposito:
-        extrato.append(f"R$ {valor:,.2f}")
-    else:
-        numero_saque += 1
-        extrato.append(f"R$ -{valor:,.2f}")
+def verifica_valor(valor) -> bool:
+    nao_valido = valor <= 0
+    
+    if nao_valido:
+        print("Digite um valor válido.")
+    
+    return nao_valido
 
-while True:
-    opcao = input(menu)
+def busca_usuario(cpf, usuarios):
+    usuarios_encontrados = [usuario for usuario in usuarios if usuario['cpf'] == cpf]
+    return usuarios_encontrados[0] if usuarios_encontrados else None
 
-    if opcao == "d":
-        deposita()
+def cria_usuario(usuarios):
+    cpf = pede_cpf()
+    usuario = busca_usuario(cpf, usuarios)
 
-    elif opcao == "s":
-        saca()
+    if(usuario):
+        print(" O usuário já existe")
+        return
+    
+    nome = input("Informe o nome: ")
+    data_nascimento =  input("Informe a data de nascimento(DD/MM/AAAA): ")
+    endereco = input("Informe o endereço: ")
 
-    elif opcao == "e":
-        imprime_extrato()
+    usuarios.append({"nome":nome, "cpf":cpf, "data_nascimento":data_nascimento, "endereco":endereco})
 
-    elif opcao == "q":
-        break
+    print(" ##### Usuario cadastrado com sucesso ##### ")
 
-    else:
-        print("Operação inválida, por favor slecione novamente a operação desejada.")
-        
+def pede_cpf() -> str:
+    return input("Informe o cpf")
 
+def cria_conta(agencia, contas, usuarios):
+    numero_conta = len(contas) + 1
+
+    cpf = pede_cpf()
+    usuario = busca_usuario(cpf, usuarios)
+
+    if usuario:
+        contas.append({"agencia": agencia, "numero_conta":numero_conta, "usuario":usuario })
+        print("Conta criada com sucesso")
+
+def main():
+    LIMITE_VALOR_SAQUE = 500
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+    saldo = 0
+    extrato = []
+    numero_saques = 0
+    usuarios = []
+    contas = []
+
+    menu = """
+    ****** MENU ******
+
+        [d]  Depositar
+        [s]  Saque
+        [e]  Extrato
+        [nc] Nova conta
+        [nu] Novo usuário
+        [q]  Sair
+    ******************
+    """
+
+    while True:
+        opcao = input(menu)
+
+        if opcao == "d":
+            saldo, extrato = deposita(saldo, extrato)
+
+        elif opcao == "s":
+            saldo, extrato, numero_saques = saca(saldo=saldo, extrato=extrato, limite=LIMITE_VALOR_SAQUE, numero_saques=numero_saques, limite_saques=LIMITE_SAQUES)
+
+        elif opcao == "e":
+            imprime_extrato(saldo, extrato=extrato)
+
+        elif opcao == "nc":
+            cria_conta(AGENCIA, contas, usuarios)
+
+        elif opcao == "nu":
+            cria_usuario(usuarios)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação inválida, por favor slecione novamente a operação desejada.")
+            
+main()
